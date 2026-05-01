9fe0036154f511fe5b0c4b2df253e223e5aa0525
Layla Barr <lb@Laylas-MacBook-Air.local>
2026-05-01 18:42:11 +1000

    Basic user register alongside fixed logo issue (late git push)

--- team_managers/urls.py
+++ team_managers/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# reinforcing the app name so errors don't occur related to webpage. (Url redirections stuff) 
+app_name = 'team_managers'
+
 urlpatterns = [
    path("", views.home, name="home"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,25 +1,36 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .forms import UserRegistrationForm, EmailAuthenticationForm
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        form = EmailAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
-            next_url = request.GET.get('next', reverse("team_managers:home"))
-            return HttpResponseRedirect(next_url)
-        else:
-            messages.error(request, "Invalid Credentials.")
-    return render(request, "users/login.html")
+            return redirect('team_managers:home')
+    else:
+        form = EmailAuthenticationForm(request)
+    return render(request, 'users/login.html', {'form': form})
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been created! You can now log in.")
+            return redirect('users:login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required(login_url='users:login')
+def user(request):
+    return render(request, "team_managers/home.html")
 
 def logout_view(request):
     logout(request)
