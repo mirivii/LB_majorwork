4dc3169733733f35e60749f2bdd0b29475e8aa79
Layla Barr <lb@Laylas-MacBook-Air.local>
2026-05-01 19:17:36 +1000

    Team Class model (underlined in red)

--- team_managers/models.py
+++ team_managers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=200) # team name
+    admin = models.ForeignKey(User, related_name='admin_of_team', on_delete=models.CASCADE)
+    invite_code = models.CharField(max_length=10, unique=True) # Security note: unique= true ensures each team will generate a unique code
+    members = models.ManyToManyField(User, related_name='joined_teams', blank=True) 
+
+    def __str__(self):
+        return self.name
